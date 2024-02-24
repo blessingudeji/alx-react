ce0a662601da785ccf033b7fd5182b23755c858c
Blessing Udeji <budeji@gmail.com>
2024-02-24 09:08:14 +0100

    React Immutable

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,6 @@
+import { fromJS } from 'immutable';
+
+// converts javascript object to immutable object using fromJS
+const getImmutableObject = (object) => fromJS(object);
+export default getImmutableObject;
+
