52bd2ea95767ab86408b968fd71c5ffb9e0deb69
Blessing Udeji <budeji@gmail.com>
2024-03-02 04:08:08 +0100

    Fixing errors

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,12 +1,34 @@
 const path = require("path");
 
 module.exports = {
-  mode: 'production',
-  entry: {
-    main: path.resolve(__dirname, './js/dashboard_main.js'),
-  },
+  mode: "production",
+  entry: "./js/dashboard_main.js",
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        // add css-loader ========
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        // add the file-loader =======
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            // add image-webpack-loader =====0
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+    ],
   },
 };
